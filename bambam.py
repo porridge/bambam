9996f9cafd24e22666ab1448be826d20381926d0
Bram Daams <brammeleman@gmail.com>
2012-12-23 00:05:36 +0000

    added alphababy style mouse drawing

--- bambam.py
+++ bambam.py
@@ -2,13 +2,30 @@
 import pygame, sys,os, random, string, glob
 from pygame.locals import * 
 
-global swidth, sheight
-# figure out the install base to use with image and sound loading
-progInstallBase = os.path.dirname(os.path.normpath(sys.argv[0]));
+# draw filled circle at mouse position
+def draw_dot():
+    r = 30
+    mousex, mousey = pygame.mouse.get_pos()
+    
+    dot = pygame.Surface((2 * r, 2 * r))
+    pygame.draw.circle(dot, get_color(), (r, r), r, 0)
+    dot.set_colorkey(0, pygame.RLEACCEL)
+    
+    screen.blit(dot, (mousex - .5 * r, mousey - .5 * r))
+
+
+# Return bright color varying over time
+def get_color():
+    col = Color('white');
+    
+    hue = pygame.time.get_ticks() / 100 % 360
+    col.hsva = (hue, 100, 100, 50)
+    
+    return Color(col.r, col.g, col.b)
 
 
 # Load image in data/, handling setting of the transparency color key
-def load_image(name, colorkey=None):
+def load_image(name, colorkey = None):
     fullname = os.path.join(progInstallBase, 'data', name)
     
     try:
@@ -19,9 +36,10 @@ def load_image(name, colorkey=None):
     image = image.convert()
     if colorkey is not None:
         if colorkey is -1:
-            colorkey = image.get_at((0,0))
+            colorkey = image.get_at((0, 0))
         image.set_colorkey(colorkey, RLEACCEL)
     return image, image.get_rect()
+
 
 # Load sound file in data/
 def load_sound(name):
@@ -36,6 +54,7 @@ def load_sound(name):
         raise SystemExit, message
     return sound
 
+
 # Loads a list of sounds
 def load_sounds(lst):
     result = []
@@ -43,47 +62,64 @@ def load_sounds(lst):
         result.append(load_sound(name))
     return result
 
+
 # Processes events
 def input(events, quit_pos):
-    
+    global sequence, mouse_down
     for event in events: 
         if event.type == QUIT: 
-            sys.exit(0) 
-        elif event.type == KEYDOWN or event.type == MOUSEBUTTONDOWN:
-            #print "eepos: %s" % (quit_pos)
-            
+            sys.exit(0)
+        
+        # handle keydown event     
+        elif event.type == KEYDOWN:
+            # check for words like quit
             if event.type == KEYDOWN:
-                if event.key == K_q:
-                    quit_pos = 1
-                elif ((event.key == K_u) and (quit_pos == 1)):
-                    quit_pos = 2
-                elif event.key == K_i and quit_pos == 2:
-                    quit_pos = 3
-                elif event.key == K_t and quit_pos == 3:
-                    sys.exit(0)
-                else:
-                    quit_pos = 0
+                if is_alpha(event.key):
+                    sequence += chr(event.key)
+                    if sequence.find('quit') > -1:
+                        sys.exit(0)
 
             # Clear the background 10% of the time
             if random.randint(0, 10) == 1:
                 screen.blit(background, (0, 0))
                 pygame.display.flip()
-            sounds[random.randint(0, len(sounds) -1)].play()
-
-            if event.type == MOUSEBUTTONDOWN or not(is_alpha(event.key)):
-                print_image()
-            else:
-                print_letter(event.key)
             
-            pygame.display.flip() 
+            # play random sound
+            sounds[random.randint(0, len(sounds) - 1)].play()
+            
+            # show images 
+            if is_alpha(event.key):
+                print_letter(event.key)
+            else:
+                print_image()
+            pygame.display.flip()
+            
+        # mouse motion
+        elif event.type == MOUSEMOTION :
+            if mouse_down:
+                draw_dot()
+                pygame.display.flip()
+        
+        # mouse button down
+        elif event.type == MOUSEBUTTONDOWN:
+            draw_dot()
+            mouse_down = 1
+            pygame.display.flip()
+
+        # mouse button up
+        elif event.type == MOUSEBUTTONUP:
+            mouse_down = 0
+        
     return quit_pos
+
 
 # Prints an image at a random location
 def print_image():
-    img = images[random.randint(0, len(images) -1)]
-    w = random.randint(0, swidth - img.get_width())
+    #global swidth, sheigh
+    img = images[random.randint(0, len(images) - 1)]
+    w = random.randint(0, swidth  - img.get_width())
     h = random.randint(0, sheight - img.get_height())
-    screen.blit(img, (w, h)) 
+    screen.blit(img, (w, h))
 
 # Is the key that was pressed alphanumeric
 def is_alpha(key):
@@ -92,7 +128,7 @@ def is_alpha(key):
 # Prints a letter at a random location
 def print_letter(key):
     font = pygame.font.Font(None, 256)
-    text = font.render(chr(key), 1, colors[random.randint(0, len(colors) -1)])
+    text = font.render(chr(key), 1, colors[random.randint(0, len(colors) - 1)])
     textpos = text.get_rect()
     center = (textpos.width / 2, textpos.height / 2)
     w = random.randint(0 + center[0], swidth - center[0])
@@ -108,8 +144,11 @@ if not pygame.mixer: print 'Warning, sound disabled'
  
 pygame.init()
 
+# figure out the install base to use with image and sound loading
+progInstallBase = os.path.dirname(os.path.normpath(sys.argv[0]));
+
 # swith to full screen at current screen resolution 
-window = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+window = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 
 # determine display resolution
 displayinfo = pygame.display.Info()
@@ -122,9 +161,11 @@ screen = pygame.display.get_surface()
 background = pygame.Surface(screen.get_size())
 background = background.convert()
 background.fill((250, 250, 250))
-
+sequence = ""
 screen.blit(background, (0, 0))
 pygame.display.flip()
+
+mouse_down = 0
 
 sounds = load_sounds(glob.glob(os.path.join(progInstallBase, 'data', '*.wav')))
 
@@ -135,10 +176,10 @@ colors = ((  0,   0, 255), (255,   0,   0), (255, 255,   0),
 
 images = (load_image("chimp.bmp", -1)[0],load_image('alien1.gif')[0])
 
-quit_pos = 0   
+quit_pos = 0
 
 clock = pygame.time.Clock()
+
 while True:
     clock.tick(60)
-    quit_pos = input(pygame.event.get(), quit_pos) 
-
+    quit_pos = input(pygame.event.get(), quit_pos)
