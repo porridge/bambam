aa4bd13a53e3cf2c56f1a5aa1176e6e23b6a73b6
marcin@owsiany.pl <marcin@owsiany.pl@fbade97f-7d42-0410-bd2a-cbe02f7fd354>
2014-02-12 20:45:23 +0000

    changed license to GPLv3 (with permission from original author), added COPYING., by Spike Burch

--- trunk/bambam.py
+++ trunk/bambam.py
@@ -1,3 +1,17 @@
+#Copyright (C) 2007-2008 Don Brown 2010 Spike Burch <spikeb@gmail.com>
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 #!/usr/bin/python
 import pygame, sys,os, random, string, glob
 from pygame.locals import * 
