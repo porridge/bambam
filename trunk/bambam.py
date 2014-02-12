db4fa8d3cecbf074f56f2e6a0a7d4e3878d09a60
marcin@owsiany.pl <marcin@owsiany.pl@fbade97f-7d42-0410-bd2a-cbe02f7fd354>
2014-02-12 20:45:35 +0000

    debian packaging added by ripps, by Spike Burch

--- trunk/bambam.py
+++ trunk/bambam.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 #Copyright (C) 2007-2008 Don Brown 2010 Spike Burch <spikeb@gmail.com>
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
@@ -12,7 +13,6 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-#!/usr/bin/python
 import pygame, sys,os, random, string, glob
 from pygame.locals import * 
 
